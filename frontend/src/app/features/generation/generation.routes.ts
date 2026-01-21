277f65165ead2ef4eaa1f439e6566a3857728272
sgorski00 <sebastiangorski00@gmail.com>
2026-01-21 17:44:24 +0100

    fix: update import in generation routes

--- frontend/src/app/features/generation/generation.routes.ts
+++ frontend/src/app/features/generation/generation.routes.ts
@@ -1,9 +1,9 @@
 import {Routes} from "@angular/router";
-import {GeneratePageComponent} from "./pages/generate-page/generate-page.component";
+import {GeneratePage} from "./generate-page/generate-page";
 
 export const generationRoutes: Routes = [
     {
         path: '',
-        component: GeneratePageComponent
+        component: GeneratePage
     }
 ]
