a4ed783ecdf10c29d95010e957117613e49a45c0
sgorski00 <sebastiangorski00@gmail.com>
2026-01-22 13:44:04 +0100

    feat: implement @for and @case in dynamic form

--- frontend/src/app/features/generation/dynamic-form/dynamic-form.ts
+++ frontend/src/app/features/generation/dynamic-form/dynamic-form.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {TitleCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-dynamic-form',
-  imports: [],
+  imports: [
+    TitleCasePipe
+  ],
   templateUrl: './dynamic-form.html',
   styleUrl: './dynamic-form.scss',
 })
 export class DynamicForm {
+  protected readonly tabs = ['general', 'clothes', 'electronics'];
+  protected readonly activeTab = signal(0);
 
+  protected setActiveTab(index: number) {
+    this.activeTab.set(index);
+  }
 }
